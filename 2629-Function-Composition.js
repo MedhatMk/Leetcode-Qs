9eedd46bd69381c19e27842e80225513debbee01
Medhat Mohamed <matouk7@outlook.com>
2024-05-14 15:28:54 +0300

    [2629] [Time Beats: 71.95%] [Memory Beats: 30.13%] - LeetPush

--- 2629-Function-Composition.js
+++ 2629-Function-Composition.js
@@ -5,11 +5,10 @@
 var compose = function(functions) {
     
     return function(x) {
-        let res = x;
        for(i = functions.length -1; i>=0; i--){
-            res = functions[i](res);
+            x = functions[i](x);
         }
-        return res;
+        return x;
     }
 };
 
